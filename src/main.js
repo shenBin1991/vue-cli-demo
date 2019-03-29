0b50f08c26040623c26693f274a83574cf66b7f9
shenBin <793388986@qq.com>
2019-03-29 17:41:06 +0800

    add element

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import i18n from './i18n'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <span>
+      {{$t('message',{name:'沈先生'})}}
+    </span>
+    <span>
+      {{$tc('car',1)}}
+    </span>
+    <span>
+      {{$tc('car',2)}}
+    </span>
   </div>
 </template>
